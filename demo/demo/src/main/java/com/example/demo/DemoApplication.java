38dabee3e4f6ac03f0fb58ba8bed1353a9c4e7e2
Andy-Piquionne <andy.piquionne@efrei.net>
2026-02-04 10:03:11 +0100

    Dockerfile

--- demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,20 @@
-package com.example.demo;
+package com.example.dockerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+@RestController
+public class DockerDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
